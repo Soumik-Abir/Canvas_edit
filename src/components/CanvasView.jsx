20a216bace20ebf35e8e752c5b214a2d9b5e5f4a
Soumik Mukherjee <mukherjeesoumik700@gmail.com>
2024-03-26 15:13:24 +0530

    Add canvas width and height state variables

--- src/components/CanvasView.jsx
+++ src/components/CanvasView.jsx
@@ -1,15 +1,17 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import CanvasEdit from "./CanvasEdit.js";
 
 const CanvasView = React.forwardRef(
   ({ captionText, ctaText, maskImage, logoImage, backgroundColor }, ref) => {
     const canvasRef = useRef(null);
+    const [canvasWidth, setCanvasWidth] = useState(1080); 
+    const [canvasHeight, setCanvasHeight] = useState(1080);
 
     useEffect(() => {
       // Adjust canvas size when component dimensions change
       const canvas = canvasRef.current;
-      canvas.width = 1080;
-      canvas.height = 1080;
+      canvas.width = canvasWidth;
+      canvas.height = canvasHeight;
 
       // Trigger canvas redraw
       const canvasDrawer = new CanvasEdit(canvas);
@@ -24,7 +26,7 @@ const CanvasView = React.forwardRef(
       return () => {
         canvasDrawer.clear();
       };
-    }, [ backgroundColor, captionText, ctaText, maskImage, logoImage]);
+    }, [canvasWidth, canvasHeight, backgroundColor, captionText, ctaText, maskImage, logoImage]);
 
     return (
       <canvas
